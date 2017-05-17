b006e24f3e85bbed85dbc15d2ef6ed01736392a9
co2y <co2y@foxmail.com>
2017-05-17 11:14:24 +0800

    init master

---
+++ master/src/main/java/me/co2y/demo/springbatch/remotepartition/MasterApplication.java
@@ -0,0 +1,20 @@
+package me.co2y.demo.springbatch.remotepartition;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Created by co2y on 17/05/2017.
+ */
+@SpringBootApplication
+@EnableBatchProcessing
+@EnableScheduling
+@ImportResource("classpath:master-config.xml")
+public class MasterApplication {
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(MasterApplication.class);
+    }
+}
---
+++ master/src/main/java/me/co2y/demo/springbatch/remotepartition/SampleIncrementer.java
@@ -0,0 +1,7 @@
+package me.co2y.demo.springbatch.remotepartition;
+
+/**
+ * Created by co2y on 17/05/2017.
+ */
+public class SampleIncrementer {
+}
